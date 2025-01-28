8cfc79ae9d35f81a9872e3fc708cbd1433e70a29
corbanevans <114201092+corbanevans@users.noreply.github.com>
2025-01-27 20:31:13 -0500

    Learning02 Final Commit

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -7,7 +7,7 @@ public class Job
     public int _startYear;
     public int _endYear;
 
-    public void DisplayAttribute()
+    public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
