a1e7eb53dcdedb143c9bc0557f3221e408315c64
corbanevans <114201092+corbanevans@users.noreply.github.com>
2025-01-27 19:50:23 -0500

    Learning02 Initial Commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+
+        job2._company = "Apple";
     }
 }
